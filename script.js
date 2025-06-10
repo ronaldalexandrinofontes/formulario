69af4c962c07bd1236349a7aa92cbd0cc9c61824
ronaldalexandrinofontes <alexandrino937@gmail.com>
2025-06-10 07:30:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+  const passwordIcons = document.querySelectorAll('.password-icon');
+
+  passwordIcons.forEach(icon => {
+
+    icon.addEventListener('click', function () { 
+
+        const input = this.parentElement.querySelector('.form_control');
+
+        input.type = input.type === 'password' ? 'text' : 'password'; 
+
+        this.classList.toggle('fa-eye');
+    })
+ })
